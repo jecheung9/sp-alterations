617961f7701858e40402190719ea6c9d72b5f7cb
jecheung9 <jeffcheung424@gmail.com>
2026-02-14 17:45:59 -0800

    #15 express and mongodb setup

---
+++ backend/src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectMongo } from "./connectMongo";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+    res.send("Hello World!");
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+})
