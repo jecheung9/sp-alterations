50a9081ca3bbb1fce7bdfcc51ca6b672a47a6148
jecheung9 <jeffcheung424@gmail.com>
2026-04-19 01:45:08 -0700

    #29 navbar active indicator

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from "react-router-dom";
+import { NavLink , useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHouse, faCalendar, faHandshake } from "@fortawesome/free-regular-svg-icons";
 import { faGear, faList, faMoneyBill1Wave, faPlus } from "@fortawesome/free-solid-svg-icons";
@@ -23,34 +23,46 @@ const Navbar: React.FC<NavbarProps> = ({
       <div className="flex justify-between">
         <h3 className="text-xl wide:text-2xl hidden sm:inline">Alterations Dashboard</h3>
       </div>
-        <Link to="/dashboard" className="flex gap-4 no-underline text-white py-2 hover:text-black hover:cursor-pointer active:text-[#000077]">
+      <NavLink to="/dashboard" className={({ isActive }) =>
+        `flex gap-4 no-underline text-white py-2
+        ${isActive ? "bg-blue-700" : "hover:text-black"}`}>
           <FontAwesomeIcon className="icon text-2xl" icon={faHouse} />
           <span className="hidden sm:inline">Dashboard</span>
-        </Link>
-        <Link to="/money" className="flex gap-4 no-underline text-white py-2 hover:text-black hover:cursor-pointer active:text-[#000077]">
+        </NavLink>
+        <NavLink to="/money" className={({ isActive }) =>
+          `flex gap-4 no-underline text-white py-2
+          ${isActive ? "bg-blue-700" : "hover:text-black"}`}>
           <FontAwesomeIcon className="icon text-2xl" icon={faMoneyBill1Wave} />
           <span className="hidden sm:inline">Money</span>
-        </Link>
-        <Link to="/calendar" className="flex gap-4 no-underline text-white py-2 hover:text-black hover:cursor-pointer active:text-[#000077]">
+        </NavLink>
+        <NavLink to="/calendar" className={({ isActive }) =>
+          `flex gap-4 no-underline text-white py-2
+          ${isActive ? "bg-blue-700" : "hover:text-black"}`}>
           <FontAwesomeIcon className="icon text-2xl" icon={faCalendar} />
           <span className="hidden sm:inline">Calendar</span>
-        </Link> 
-        <Link to="/todo" className="flex gap-4 no-underline text-white py-2 hover:text-black hover:cursor-pointer active:text-[#000077]">
+        </NavLink> 
+        <NavLink to="/todo" className={({ isActive }) =>
+          `flex gap-4 no-underline text-white py-2
+          ${isActive ? "bg-blue-700" : "hover:text-black"}`}>
           <FontAwesomeIcon className="icon text-2xl" icon={faList} />
           <span className="hidden sm:inline">To-do</span>
-      </Link>
-      <Link to="/meetings" className="flex gap-4 no-underline text-white py-2 hover:text-black hover:cursor-pointer active:text-[#000077]">
+      </NavLink>
+      <NavLink to="/meetings" className={({ isActive }) =>
+        `flex gap-4 no-underline text-white py-2
+        ${isActive ? "bg-blue-700" : "hover:text-black"}`}>
         <FontAwesomeIcon className="icon text-2xl" icon={faHandshake} />
         <span className="hidden sm:inline">Meetings</span>
-      </Link>
+      </NavLink>
       <div className="flex gap-4 no-underline text-white py-2 hover:text-black hover:cursor-pointer active:text-[#000077]" onClick={onOpen} >
         <FontAwesomeIcon className="icon text-2xl" icon={faPlus} />
         <span className="hidden sm:inline">Add Entry</span>
       </div>
-      <Link to="/settings" className="flex gap-4 no-underline text-white py-2 hover:text-black hover:cursor-pointer active:text-[#000077]">
+      <NavLink to="/settings" className={({ isActive }) =>
+        `flex gap-4 no-underline text-white py-2
+        ${isActive ? "bg-blue-700" : "hover:text-black"}`}>
         <FontAwesomeIcon className="icon text-2xl" icon={faGear} />
         <span className="hidden sm:inline">Settings</span>
-      </Link>
+      </NavLink>
 
       {token && (
         <div className="navbar-auth">
