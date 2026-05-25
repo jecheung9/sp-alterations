995314078dd2fdefe2f1ad6a46b686cd7c231a22
jecheung9 <jeffcheung424@gmail.com>
2026-05-25 01:16:56 -0700

    #41 setup playwright ci

--- tests/landing.spec.ts
+++ tests/landing.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 test("login page loads", async ({ page }) => {
-    await page.goto('http://localhost:5173');
+    await page.goto('/');
     await expect(page).toHaveTitle(/vite/i);
 })
 
