fb6f675f155e2122d7edbb2acbfac0205479bb92
jecheung9 <jeffcheung424@gmail.com>
2026-05-25 01:02:29 -0700

    #42 frontend testing setup with playwright

---
+++ tests/landing.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from "@playwright/test";
+
+test("login page loads", async ({ page }) => {
+    await page.goto('http://localhost:5173');
+    await expect(page).toHaveTitle(/vite/i);
+})
+
